2f354aaabf337ce80a0bedc82bf01aa21bd2d046
Jacob Cassidy <jacob@jacobcassidy.com>
2025-01-04 07:12:13 +0700

    Format files with spaces

--- prettier.config.js
+++ prettier.config.js
@@ -3,18 +3,18 @@
  * @type {import("prettier").Config}
  */
 const config = {
-	plugins: [ 'prettier-plugin-multiline-arrays' ],
-	overrides: [
-		{
-			files: '*.json',
-			options: {
-				multilineArraysWrapThreshold: 0,
-			},
-		},
-	],
-	experimentalTernaries: true,
-	printWidth: 120,
-	singleQuote: true,
+  plugins: ['prettier-plugin-multiline-arrays'],
+  overrides: [
+    {
+      files: '*.json',
+      options: {
+        multilineArraysWrapThreshold: 0,
+      },
+    },
+  ],
+  experimentalTernaries: true,
+  printWidth: 120,
+  singleQuote: true,
 };
 
 export default config;
