c0fa389770c22771d94287d067dae4234e22d173
Jacob Cassidy <jacob@jacobcassidy.com>
2025-01-04 07:12:44 +0700

    Update file description comment

--- tests/compare.js
+++ tests/compare.js
@@ -1,21 +1,23 @@
-// Checks if all properties in the css-modules directory are included in the propertiesOrder array
+/**
+ * Checks if all properties in the `css-modules` directory are included in the propertiesOrder array.
+ */
 import * as cssModules from '../css-modules/index.js';
 import propertiesOrder from '../config.js';
 
-const allProps = createFlatArray( cssModules );
-const orderedProps = createFlatArray( propertiesOrder );
+const allProps = createFlatArray(cssModules);
+const orderedProps = createFlatArray(propertiesOrder);
 
-const missingProps = allProps.filter( ( value ) => ! orderedProps.includes( value ) );
+const missingProps = allProps.filter((value) => !orderedProps.includes(value));
 
-function createFlatArray( obj ) {
-	const flatArray = [];
+function createFlatArray(obj) {
+  const flatArray = [];
 
-	for ( const key in obj ) {
-		const values = Object.values( obj[ key ] );
-		flatArray.push( ...values );
-	}
-	return flatArray.flat( Infinity );
+  for (const key in obj) {
+    const values = Object.values(obj[key]);
+    flatArray.push(...values);
+  }
+  return flatArray.flat(Infinity);
 }
 
 // eslint-disable-next-line no-console
-console.log( 'Missing Props:', missingProps );
+console.log('Missing Props:', missingProps);
