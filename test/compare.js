b7fb7d585b38c22a0cf900295999b9e64815c542
Jacob Cassidy <jacob@jacobcassidy.com>
2025-01-09 09:28:01 +0700

    Update output list

--- test/compare.js
+++ test/compare.js
@@ -1,11 +1,11 @@
 /**
  * Checks if all properties in the `css-modules` directory are included in the propertiesOrder array.
  */
-import * as cssModules from '../css-modules/index.js';
+import * as cssModules from '../lib/css-modules/index.js';
 import propertiesOrder from '../config.js';
 
 const allProps = createFlatArray(cssModules);
-const orderedProps = createFlatArray(propertiesOrder);
+const orderedProps = createFlatArray(propertiesOrder.rules['order/properties-order'][0]);
 
 const missingProps = allProps.filter((value) => !orderedProps.includes(value));
 
