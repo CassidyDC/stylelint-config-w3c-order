807972b07d5aad25a3df83ef8657be876d9cec34
Jacob Cassidy <jacob@jacobcassidy.com>
2025-08-15 09:56:51 +0700

    Add `eslint-plugin-import-x` package and extend config

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import { importX } from 'eslint-plugin-import-x';
 
 export default defineConfig([
   globalIgnores([
@@ -24,10 +25,12 @@ export default defineConfig([
   ]),
   {
     plugins: {
+      'import-x': importX,
       js,
       tseslint,
     },
     extends: [
+      'import-x/recommended',
       'js/recommended',
       'tseslint/recommended',
     ],
